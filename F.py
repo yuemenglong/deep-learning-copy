f562e6f3ab7160bfbba7607fb8f0c5d844b626b0
yuemenglong <yuemenglong@126.com>
2019-04-08 17:10:33 +0800

    add: skip_no_face

--- F.py
+++ F.py
@@ -35,3 +35,31 @@ def angle_between(v1, v2):
     v1_u = unit_vector(v1)
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+
+
+def skip_no_face(dir, pat="%05d"):
+    import os
+    import shutil
+    aligend_dir = os.path.join(dir, "aligned")
+    aligend = set([f.split("_")[0] for f in os.listdir(aligend_dir)])
+    merged_dir = os.path.join(dir, "merged")
+    merged_dir_bak = os.path.join(dir, "merged_bak")
+    if os.path.exists(merged_dir_bak):
+        raise Exception("Merge Dir Bak Exists")
+    shutil.move(merged_dir, merged_dir_bak)
+    os.mkdir(merged_dir)
+    idx = 0
+    for f in os.listdir(merged_dir_bak):
+        name = os.path.splitext(f)[0]
+        ext = os.path.splitext(f)[-1]
+        if name in aligend:
+            idx += 1
+            print(idx)
+            src = os.path.join(merged_dir_bak, f)
+            dst = os.path.join(merged_dir, "%05d" % idx + ext)
+            shutil.move(src, dst)
+
+
+if __name__ == '__main__':
+    dir = "D:/DeepFaceLabCUDA9SSE/workspace/data_dst"
+    skip_no_face(dir)
