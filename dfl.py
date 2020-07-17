98d744a848f5238c5285640bfdb2023525188794
yuemenglong <yuemenglong@126.com>
2020-07-17 08:36:04 +0800

    add: output_debug显示指定

--- dfl.py
+++ dfl.py
@@ -244,11 +244,24 @@ def dfl_extract_faces(input_dir, output_dir, detector="s3fd", manual_fix=False, 
     args = {
         "--input-dir": input_dir,
         "--output-dir": output_dir,
-        "--output-debug": output_debug,
+        # "--output-debug": output_debug,
         "--manual-fix": manual_fix,
         "--detector": detector,
         "--face-type": "whole_face",
+        "--max-faces-from-image": 3,
+        "--image-size": 512,
+        "--jpeg-quality": 90,
+        # p.add_argument('--output-debug', action="store_true", dest="output_debug", default=None, help="Writes debug images to <output-dir>_debug\ directory.")
+        # p.add_argument('--no-output-debug', action="store_false", dest="output_debug", default=None, help="Don't writes debug images to <output-dir>_debug\ directory.")
+        # p.add_argument('--face-type', dest="face_type", choices=['half_face', 'full_face', 'whole_face', 'head', 'mark_only'], default=None)
+        # p.add_argument('--max-faces-from-image', type=int, dest="max_faces_from_image", default=None, help="Max faces from image.")
+        # p.add_argument('--image-size', type=int, dest="image_size", default=None, help="Output image size.")
+        # p.add_argument('--jpeg-quality', type=int, dest="jpeg_quality", default=None, help="Jpeg quality.")
     }
+    if output_debug:
+        args["--output-debug"] = True
+    else:
+        args["--no-output-debug"] = True
     dfl_exec(cmd, args)
 
 
