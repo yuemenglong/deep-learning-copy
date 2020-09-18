a72436e97d9dc3bd5dffe6aca601b6478a9adedf
yuemenglong <yuemenglong@126.com>
2020-09-19 07:08:51 +0800

    add: no warning

--- mainscripts/Extractor.py
+++ mainscripts/Extractor.py
@@ -859,7 +859,7 @@ def main(detector=None,
                 io.log_err("Error in fetching the last index. Extraction cannot be continued.")
                 return
         elif input_path != output_path:
-                io.input(f"\n WARNING !!! \n {output_path} contains files! \n They will be deleted. \n Press enter to continue.\n")
+                # io.input(f"\n WARNING !!! \n {output_path} contains files! \n They will be deleted. \n Press enter to continue.\n")
                 for filename in output_images_paths:
                     Path(filename).unlink()
 
