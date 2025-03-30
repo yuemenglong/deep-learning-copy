7dfdd01936774590dfbbc5bd333af03119c8c65e
yuemenglong <yuemenglong@126.com>
2025-03-30 15:09:20 +0800

    备份时间

--- models/ModelBase.py
+++ models/ModelBase.py
@@ -403,7 +403,7 @@ class ModelBase(object):
         pathex.write_bytes_safe (self.model_data_path, pickle.dumps(model_data) )
 
         if self.autobackup_hour != 0:
-            diff_hour = int ( (time.time() - self.autobackup_start_time) // 3600 )
+            diff_hour = int ( (time.time() - self.autobackup_start_time) // 1800 )
             # diff_hour = int ( (time.time() - self.autobackup_start_time) // 900 )
 
             if diff_hour > 0 and diff_hour % self.autobackup_hour == 0:
